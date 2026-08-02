00c5ead3eb12cda9c6874089e345d546d57566f8
Niloy Ahsan <niloyahsan786@gmail.com>
2026-08-03 02:06:52 +0600

    design changed

--- js/header_scroll.js
+++ js/header_scroll.js
@@ -1,7 +1,8 @@
 window.addEventListener("scroll", function () {
-    var header = document.getElementById("header");
-    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    var scrollAmount = Math.min(0.5, scrollTop / 500);
-
-    header.style.backgroundPositionY = scrollAmount * 100 + "%";
+    var header = document.querySelector("header");
+    if (header) {
+        var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        var scrollAmount = Math.min(0.5, scrollTop / 500);
+        header.style.backgroundPositionY = scrollAmount * 100 + "%";
+    }
 });
