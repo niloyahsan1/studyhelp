82399df161770dcfbb3cd03861b4803a20adae44
Niloy Ahsan <niloyahsan.official@gmail.com>
2026-08-07 20:53:29 +0600

    file deleted

--- js/page_loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("load", function () {
-    const loader = document.querySelector(".loader");
-    loader.classList.add("loader-hidden");
-    loader.addEventListener("transitioned", function () {
-        document.body.removeChild("loader");
-    });
-});
